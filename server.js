11ed1866f3931d0f3379534b124bfeda8603ef08
Darth Mirandor <darth.mirandor@gmail.com>
2020-08-29 14:43:51 -0500

    trying to fix heroku errors

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +11,6 @@ app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
